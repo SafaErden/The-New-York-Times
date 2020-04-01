17f918d79a2268d1fdb3cd8f960199114d617992
SafaErden <60184961+SafaErden@users.noreply.github.com>
2020-04-01 18:31:21 +0300

    fixed code for header switch

--- Assets/js/javascript.js
+++ Assets/js/javascript.js
@@ -1,9 +1,11 @@
+window.onscroll = function() {scrollFunction()};
+
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("header2").style.top = "0";
-        document.getElementById("header1").style.top = "-50px";
+        document.getElementById("header1").style.display = "none";
+        document.getElementById("header2").style.display = "block";
     } else {
-        document.getElementById("header2").style.top = "-50px";
-        document.getElementById("header1").style.top = "0";
+        document.getElementById("header1").style.display = "block";
+        document.getElementById("header2").style.display = "none";
     }
   }
